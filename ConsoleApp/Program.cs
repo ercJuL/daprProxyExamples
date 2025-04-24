07e1473279e21f0fc73b1b2aeea2281015bc91b8
ercjul <1552930110@qq.com>
2025-04-24 10:29:54 +0800

    use DaprClient

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using daprProxyExamples;
+﻿using Dapr.Client;
+using daprProxyExamples;
 using Grpc.Core;
 using Grpc.Net.Client;
 
@@ -8,8 +9,13 @@ var client1 = new Greeter.GreeterClient(channel1);
 var reply1 = await client1.SayHelloAsync(new HelloRequest { Name = "direct" });
 Console.WriteLine(" " + reply1.Message);
 
-Console.WriteLine("request grpc server with dapr proxy....");
-var channel2 = GrpcChannel.ForAddress("http://localhost:50001"); // dapr grpc port
-var client2 = new Greeter.GreeterClient(channel2);
-var reply2 = await client2.SayHelloAsync(new HelloRequest { Name = "proxy from dapr" }, new Metadata(){{"dapr-app-id", "grpc-server"}});
-Console.WriteLine(" " + reply2.Message);
+// Console.WriteLine("request grpc server with dapr proxy....");
+// var channel2 = GrpcChannel.ForAddress("http://localhost:50001"); // dapr grpc port
+// var client2 = new Greeter.GreeterClient(channel2);
+// var reply2 = await client2.SayHelloAsync(new HelloRequest { Name = "proxy from dapr" }, new Metadata(){{"dapr-app-id", "grpc-server"}});
+// Console.WriteLine(" " + reply2.Message);
+
+var invoker = DaprClient.CreateInvocationInvoker("grpc-server", "http://localhost:50001");
+var client3 = new Greeter.GreeterClient(invoker);
+var reply3 = client3.SayHello(new HelloRequest { Name = "proxy from dapr invoker" });
+Console.WriteLine(" " + reply3.Message);
